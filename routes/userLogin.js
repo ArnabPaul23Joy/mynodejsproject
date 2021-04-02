5e4746839b9f81e3ca15e77976fedf14fba2e875
ArnabPaul23Joy <arnabpaul65f826@gmail.com>
2021-04-03 00:09:46 +0600

    jwt commit

--- routes/userLogin.js
+++ routes/userLogin.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const jwt=require("jsonwebtoken")
 
 // require("dotenv").config()
 // const express=require("express")
@@ -11,8 +12,8 @@ const bcrypt=require("bcrypt")
 
 
 let User = require("../models/userModel.js");
-// const passport=require("passport")
-// const passportLocalMongoose=require("passport-local-mongoose")
+const passport=require("passport")
+const passportLocalMongoose=require("passport-local-mongoose")
 // const GoogleStrategy = require('passport-google-oauth20').Strategy;
 // const FacebookStrategy = require('passport-facebook').Strategy;
 // const findOrCreate = require('mongoose-findorcreate')
@@ -38,30 +39,35 @@ const md5 =require("md5")
 router.route('/').post((req, res) => {
     const uName=req.body.email
     const pword=req.body.password
-        // const user=new User({
-        //     email: req.body.email,
-        //     password: req.body.password
-        // })
-        // req.login(user,function(err){
-        //     if(err){
-        //         () => res.status(400).json('Error: ' + err);
-        //     }
-        //     else{
-                
-        //     }
-        // })
-    console.log(uName+" "+pword)
+    // const user=new User({
+    //         email: req.body.email,
+    //         password: req.body.password
+    //     })
+    //     req.login(user,function(err){
+    //         if(err){
+    //             res.json({
+    //                 status: "Wrong password bruh!"
+    //         })
+    //         }else{
+    //             passport.authenticate("local")(req,res,function(){
+    //                 res.render("secrets")
+    //             })
+    //         }
+    //     })
+    // console.log(uName+" "+pword)
         User.findOne({email: uName},function(err,foundUser){
             if(!err){
                 console.log("found user "+uName)
                 if(foundUser){
                     bcrypt.compare(pword,foundUser.password,function(err,result){
                         if (result==true){
-                            res.json({
+                            const token=jwt.sign({
                                 status: "Success",
                                 email: foundUser.email,
                                 u_id: foundUser._id
-                            })
+                            }, process.env.TOKEN_SECRET)
+                            res.header("auth-token",token).send(token)
+                            // res.json()
                         }
                         else{
                             res.json({
