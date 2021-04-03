648998b3c85d1c265b33ebd075d67fce104d5ee5
ArnabPaul23Joy <arnabpaul65f826@gmail.com>
2021-04-04 01:02:04 +0600

    testing getposts

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -34,7 +34,7 @@ function App() {
   }
   function setToken(token){
       console.log("from APP "+token)
-      axios.post('getnotes/', token)
+      axios.post('getnotes/', {token: token})
         .then(res => {console.log(res.data)});
         // setNotes(tttt.notePosts)
       } 
