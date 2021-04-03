c9fc339936bd3afa0aee26bd8d7a5f2079f9596a
ArnabPaul23Joy <arnabpaul65f826@gmail.com>
2021-04-04 01:49:45 +0600

    testing getposts

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -4,14 +4,14 @@ module.exports= function (req, res, next){
     const token=req.body.token
     // console.log("hghghgh  "+token)
     if(!token) return res.status(401).send('Access Denied')
-    try{
+    // try{
         var verified=jwt.verify(token, process.env.TOKEN_SECRET)
         console.log("verifired   "+verified)
         // req.user=verified
         next()
-    }
-    catch(err){
-        res.send("Invalid Token")
-    }
+    // }
+    // catch(err){
+        // res.send("Invalid Token")
+    // }
 
 }
