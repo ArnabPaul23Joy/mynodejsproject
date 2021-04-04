53d4d38b417f822d8020f3187cc9d5cbc444be13
ArnabPaul23Joy <arnabpaul65f826@gmail.com>
2021-04-04 14:34:02 +0600

    testing getposts

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,16 +1,10 @@
-const jwt =-require('jsonwebtoken')
+
+const jwt=require("jsonwebtoken")
 module.exports= function (req, res, next){
     // const token=req.header('auth-token')
     const token=req.body.token
     console.log("hghghgh  "+token)
     if(!token) return res.status(401).send('Access Denied')
-    jwt.verify(token, process.env.TOKEN_SECRET, function(err, decoded) {
-        
-        console.log("decoded   ")
-        console.log(decoded)
-        // req.user=verified
-        next()
-
-    })
+    console.log(jwt.verify(token, process.env.TOKEN_SECRET))
 
 }
