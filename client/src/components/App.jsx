5473b48a6965832cbcc5519517b4056fe93f1554
ArnabPaul23Joy <arnabpaul65f826@gmail.com>
2021-04-06 01:34:11 +0600

    testing getposts

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -10,34 +10,35 @@ import Register from "./Register";
 function App() {
 
   
-  // function getCookie(cname) {
-  //   var name = cname + "=";
-  //   var decodedCookie = decodeURIComponent(document.cookie);
-  //   var ca = decodedCookie.split(';');
-  //   for(var i = 0; i <ca.length; i++) {
-  //     var c = ca[i];
-  //     while (c.charAt(0) == ' ') {
-  //       c = c.substring(1);
-  //     }
-  //     if (c.indexOf(name) == 0) {
-  //       return c.substring(name.length, c.length);
-  //     }
-  //   }
-  //   return "Invalid Token";
-  // }
+  function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "Invalid Token";
+  }
 
   const [globToken, setGlobTok] = useState("InvalidToken")
   const [logIn, setLogInBox] = useState("reload");
-  // function setCookie(cname, globToken, exdays) {
-  //   var d = new Date();
-  //   d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  //   var expires = "expires="+ d.toUTCString();
-  //   document.cookie = cname + "=" + globToken + ";" + expires + ";path=/";
-  // }
+  function setCookie(cname, globToken, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + globToken + ";" + expires + ";path=/";
+  }
 
 
 
   if(logIn==="reload"){
+    console.log(getCookie("keeeppperrr"))
     // setGlobTok(getCookie("keeeppperrr"))
       axios.post('getnotes/', {token: globToken})
         .then(res => {
