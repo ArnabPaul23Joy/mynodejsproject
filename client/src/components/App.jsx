85e0a975bc84f2be88f00494b33e287cab153d56
ArnabPaul23Joy <arnabpaul65f826@gmail.com>
2021-04-20 21:53:11 +0600

    1st

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -57,8 +57,8 @@ function App() {
           }
           else{
             setLogInBox("login")
-          }
           console.log("ggg    "+res.data.status)
+          }
         });
   }
   function logInOrRegister(logValue){
