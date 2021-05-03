ce72e94ad591408aebe01dd4ac71ca8f77863d61
ArnabPaul23Joy <arnabpaul65f826@gmail.com>
2021-05-03 13:46:34 +0600

    fixing the getnotes 404 error

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+mongoose.connect("mongodb://localhost:27017/ListingAppTodo",{useNewUrlParser:true})
 const userSchema=new mongoose.Schema({
   userName: String,
   email: {
