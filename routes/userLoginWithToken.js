e48c887e0b3a34da4b95e99124a2879f6a41bb1c
ArnabPaul23Joy <arnabpaul65f826@gmail.com>
2021-05-13 19:57:37 +0600

    still trying

--- routes/userLoginWithToken.js
+++ routes/userLoginWithToken.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+const cors = require("cors");
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
