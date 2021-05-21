a934816d20ad0cf90070175ccf8334544fe71aaa
ArnabPaul23Joy <arnabpaul65f826@gmail.com>
2021-05-22 00:41:30 +0600

    fixing google

--- server.js
+++ server.js
@@ -16,7 +16,12 @@ const port = process.env.PORT||5000;
 
 // app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+// app.use(express.urlencoded({extended: false}));
 
 // app.use(express.json());
 // app.use(cookieParser());
