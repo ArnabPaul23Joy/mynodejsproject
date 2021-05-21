487328cfa030c310bbd5fbb19c1fd5fd996811ba
ArnabPaul23Joy <arnabpaul65f826@gmail.com>
2021-05-21 16:05:40 +0600

    fixing google

--- client/src/components/LogIn.jsx
+++ client/src/components/LogIn.jsx
@@ -87,32 +87,74 @@ function LogIn(props){
                 <form className="form-signin" onSubmit={handleSignIn}>
                   <div className="form-label-group">
                     <label for="inputEmail">Email address</label>
-                    <input type="email" id="inputEmail" name="email" value={user.email} className="form-control" onChange={handleChange} placeholder="Email address" required autofocus />
+                    <input
+                      type="email"
+                      id="inputEmail"
+                      name="email"
+                      value={user.email}
+                      className="form-control"
+                      onChange={handleChange}
+                      placeholder="Email address"
+                      required
+                      autofocus
+                    />
                   </div>
-
                   <div className="form-label-group passwordField">
                     <label for="inputPassword">Password</label>
-                    <input type="password" id="inputPassword" name="password" value={user.password} className="form-control" onChange={handleChange} placeholder="Password" required />
-                  </div><br/>
-
+                    <input
+                      type="password"
+                      id="inputPassword"
+                      name="password"
+                      value={user.password}
+                      className="form-control"
+                      onChange={handleChange}
+                      placeholder="Password"
+                      required
+                    />
+                  </div>
+                  <br />
                   {/* <div className="custom-control custom-checkbox mb-3">
                     <input type="checkbox" className="custom-control-input" id="customCheck1" />
                     <label className="custom-control-label" for="customCheck1">Remember password</label>
                   </div> */}
-                  <button className="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
-                  <hr className="my-4"/>
+                  <button
+                    className="btn btn-lg btn-primary btn-block text-uppercase"
+                    type="submit"
+                  >
+                    Sign in
+                  </button>
+                  <hr className="my-4" />
                   {/* onClick={handleGoogleSignIn} */}
-                  <button className="btn btn-lg btn-google btn-block text-uppercase" onClick={handleGoogleSignIn}><i className="fab fa-google mr-2"></i> Sign in with Google</button>
+                  <button
+                    className="btn btn-lg btn-google btn-block text-uppercase"
+                    onClick={handleGoogleSignIn}
+                  >
+                    <a href="/auth/google" role="button">
+                      <i className="fab fa-google mr-2"></i> Sign in with Google
+                    </a>
+                  </button>
                   {/* onClick={handleFacebookSignIn} */}
-                  <button className="btn btn-lg btn-facebook btn-block text-uppercase"><i className="fab fa-facebook-f mr-2"></i> Sign in with Facebook</button>
-                  <br/>
-                  <p>Do not have an account? <button data-testid="switch-to-login" onClick={handleClick}class="registerBtton">Sign Up</button></p>
+                  <button className="btn btn-lg btn-facebook btn-block text-uppercase">
+                    <i className="fab fa-facebook-f mr-2"></i> Sign in with
+                    Facebook
+                  </button>
+                  <br />
+                  <p>
+                    Do not have an account?{" "}
+                    <button
+                      data-testid="switch-to-login"
+                      onClick={handleClick}
+                      class="registerBtton"
+                    >
+                      Sign Up
+                    </button>
+                  </p>
                 </form>
               </div>
             </div>
           </div>
         </div>
-      </div>      
+      </div>
     </div>
   );
 }
