b6ef6efd1ad20ddefc42cc7a158163ee187f4db8
ArnabPaul23Joy <arnabpaul65f826@gmail.com>
2021-05-22 00:43:24 +0600

    fixing google

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 // const cors = require("cors");
 
 const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
 var crypto = require("crypto");
 const cookieParser = require("cookie-parser");
 // const path = require("path");
@@ -16,12 +17,9 @@ const port = process.env.PORT||5000;
 
 // app.use(cors());
 app.use(express.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-// app.use(express.urlencoded({extended: false}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: false}));
 
 // app.use(express.json());
 // app.use(cookieParser());
