3e0bb2c28e0e0e7f535a373e819789892c24695d
ArnabPaul23Joy <arnabpaul65f826@gmail.com>
2021-05-25 22:49:08 +0600

    google login

--- routes/LoginGoogle.jsx
+++ routes/LoginGoogle.jsx
@@ -73,24 +73,64 @@ router.get("/", function (req, res) {
       { googleId: payload["sub"] },
       function (err, usr) {
         console.log("err  " + err);
-        console.log("usr  " + usr);
-      }
-    );
-    
-    console.log("nUsr   " + nUsr);
-    if (!nUsr) {
-      stttt += payload["email"] + payload["sub"];
-      var hash = crypto.createHash("md5").update(stttt).digest("hex");
-      newUser = new User({
-        userName: payload["name"],
-        email: payload["email"],
-        password: hash,
-        googleId: payload["sub"],
-      });
-      await newUser.save(async function (err) {
-        if (!err) {
+        console.log("nUsr   " + nUsr);
+        if (!nUsr) {
+          stttt += payload["email"] + payload["sub"];
+          var hash = crypto.createHash("md5").update(stttt).digest("hex");
+          newUser = new User({
+            userName: payload["name"],
+            email: payload["email"],
+            password: hash,
+            googleId: payload["sub"],
+          });
+          await newUser.save(async function (err) {
+            if (!err) {
+              var u_iid = "";
+              u_iid += newUser._id.toString();
+              u_iid = crypto.createHash("md5").update(u_iid).digest("hex");
+              var rFieldVal =
+                u_iid + Math.random().toString(36).substring(7) + u_iid;
+              rFieldVal = crypto
+                .createHash("md5")
+                .update(rFieldVal)
+                .digest("hex");
+              const token = jwt.sign(
+                {
+                  status: "Success",
+                  u_id: newUser._id,
+                  [u_iid]: rFieldVal,
+                },
+                process.env.TOKEN_SECRET
+              );
+
+              console.log("userRegister   " + token);
+
+              res.cookie("token", token, { httpOnly: true });
+              res.send({ status: "Successful", token: token });
+              await randNumber.updateOne(
+                { u_idHash: u_iid },
+                { jToken: token },
+                { upsert: true },
+                function (err, docs) {
+                  if (err) {
+                    console.log(err);
+                    res.send({ status: "Update Failed" });
+                  } else {
+                    console.log("Original Doc : ", docs);
+                  }
+                }
+              );
+            } else {
+              return res.send("Wrong email or password!");
+            }
+          });
+        }
+
+        else{
+
+
           var u_iid = "";
-          u_iid += newUser._id.toString();
+          u_iid += nUsr._id.toString();
           u_iid = crypto.createHash("md5").update(u_iid).digest("hex");
           var rFieldVal =
             u_iid + Math.random().toString(36).substring(7) + u_iid;
@@ -98,7 +138,7 @@ router.get("/", function (req, res) {
           const token = jwt.sign(
             {
               status: "Success",
-              u_id: newUser._id,
+              u_id: nUsr._id,
               [u_iid]: rFieldVal,
             },
             process.env.TOKEN_SECRET
@@ -121,11 +161,12 @@ router.get("/", function (req, res) {
               }
             }
           );
-        } else {
-          return res.send("Wrong email or password!");
+
         }
-      });
-    }
+      }
+    );
+    
+    
 
     // If request specified a G Suite domain:
     // const domain = payload['hd'];
