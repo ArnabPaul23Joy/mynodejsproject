b7652fcbae840e19fd30dba23e3a30b403c2ed99
ArnabPaul23Joy <arnabpaul65f826@gmail.com>
2021-06-13 18:01:33 +0600

    emailconfirm

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -204,7 +204,7 @@ function App() {
             </div>
           )}
         />
-        <Route
+        {/* <Route
           path="/"
           exact
           component={() => (
@@ -239,22 +239,10 @@ function App() {
               ) : (
                 <div></div>
               )}
-              {/* <CreateArea onAdd={addNote} />
-                {notes.map((noteItem, index) => {
-                  return (
-                    <Note
-                      key={index}
-                      id={index}
-                      title={noteItem.title}
-                      content={noteItem.content}
-                      onDelete={deleteNote}
-                    />
-                  );
-                })} */}
               <Footer />
             </div>
           )}
-        />
+        /> */}
         <Route
           path="/confirmation"
           exact
