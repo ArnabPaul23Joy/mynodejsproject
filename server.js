9aa81ac9c5eded01a898b28aff3d558770fc2330
ArnabPaul23Joy <arnabpaul65f826@gmail.com>
2021-06-13 21:55:59 +0600

    emailconfirm

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const md5 = require("md5");
 // app.use(passport.session());
 // const cors = require("cors");
 // const cookieParser = require("cookie-parser");
-// const path = require("path");
+const path = require("path");
 // app.use(cors({credentials: true }));
 // app.use(express.json());
 app.use(cookieParser());
@@ -83,109 +83,26 @@ app.use('/logout', logOut);
 app.use("/loginWithToken", userLoginWithToken)
 app.use("/verify", verify);
 app.use("/confirmation", confirmation);
-// app.use(passport.initialize());
-// app.use(passport.session());
-// app.get('/*', function (req, res) {
-//    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-//  });
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname +'/../client/public/index.html'))
-// });
 
-// if (process.env.NODE_ENV === 'production') {
-  // console.log("production mode!")
-  // Exprees will serve up production assets
-  app.use(express.static('client/build'));
+app.use(express.static("client/build"));
 
-  // Express serve up index.html file if it doesn't recognize route
-//   const path = require('path');
-//   app.get('/', (req, res) => {
-//     console.log("pro mode!")
-//     res.sendFile('client/build/index.html');
-//   });
-// }
-// var newUser= new User()
-// passport.use(User.createStrategy());
-
-// passport.serializeUser(User.serializeUser());
-// passport.deserializeUser(User.deserializeUser());
-var newUser;
-// passport.serializeUser(function (user, done) {
-//   done(null, user.id);
-// });
-
-// passport.deserializeUser(function (id, done) {
-//   User.findById(id, function (err, user) {
-//     done(err, user);
-//   });
-// });
-
-// passport.use(
-//   new GoogleStrategy(
-//     {
-//       clientID: process.env.CLIENT_ID,
-//       clientSecret: process.env.CLIENT_SECRET,
-//       callbackURL: "http://localhost:5000/callback",
-//       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
-//     },
-//     function (accessToken, refreshToken, profile, cb) {
-//       console.log(profile.emails[0].value);
-//       var stttt = "";
-//       stttt += profile.emails[0].value + profile.id;
-//       var hash = crypto.createHash("md5").update(stttt).digest("hex");
-//       newUser = new User({
-//         userName: profile.displayName,
-//         email: profile.emails[0].value,
-//         password: hash,
-//         googleId: profile.id,
-//       });
-//       // bcrypt.genSalt(10, async function (err, salt) {
-//       //   bcrypt.hash(stttt, salt, async function (err, hash) {
-
-//       //   })
-//       // })
-//       User.findOrCreate({ googleId: newUser.googleId }, function (err, user) {
-//         //findOrCreate isn't a mongo db function
-//         // newUser.
-
-//         // if (err){
-//         //   return cb(err, user);
-//         // }
-//         // else{
-//         // if (!user){
-
-//         // }
-//         console.log(newUser);
-//         console.log("user  ", user);
-//         return cb(err, newUser);
-//         // }
-//       });
-//     }
-//   )
-// );
-
-// app.get(
-//   "/",
-//   passport.authenticate("google", { scope: ["profile","email"] })
-// );
-
-// app.get(
-//   "/callback",
-//   passport.authenticate("google", { failureRedirect: "/login" }),
-//   function (req, res) {
-//     // Successful authentication, redirect home.
-//     // console.log("user ", user);
-//     // console.log("user ", req.user);
-//     res.send({ status: "testing", newUser: newUser });
-//   }
-// );
+app.get("/", function (req, res) {
+  res.sendFile("index.html", {
+    root: path.join(__dirname, "../../client/build/"),
+  });
+});
+app.get("/about", function (req, res) {
+  res.sendFile("index.html", {
+    root: path.join(__dirname, "../../client/build/"),
+  });
+});
+app.get("/confirmation", function (req, res) {
+  res.sendFile("index.html", {
+    root: path.join(__dirname, "../../client/build/"),
+  });
+});
 console.log(process.env.NODE_ENV);
-  // process.env.ChocolateyLastPathUpdate
-  
-  // ;
-
-
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
