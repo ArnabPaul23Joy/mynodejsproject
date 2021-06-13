8b4ec62905ccac37cf21ffb459e9d160235bf633
ArnabPaul23Joy <arnabpaul65f826@gmail.com>
2021-06-13 22:05:59 +0600

    emailconfirm

--- routes/verify.js
+++ routes/verify.js
@@ -81,54 +81,56 @@ router.route("/").post(async (req, res) => {
   var tempRand=req.query.rFieldVal
   console.log(emHash,tempRand)
   await TemporaryUserToken.findOne({ emHash: emHash }, async function (err, tempUserToken){
-    if(tempUserToken.tempRand===tempRand){
+    if(tempUserToken.tempRand===tempRand && tempRand!== 'undefined'){
       const newUser = new User({
         userName: tempUserToken.uName,
         email: tempUserToken.tempEmail,
         password: tempUserToken.password,
       });
+      await newUser.save(async function (err) {
+        if (!err) {
+          var u_iid = "";
+          u_iid += newUser._id.toString();
+          u_iid = crypto.createHash("md5").update(u_iid).digest("hex");
+          var rFieldVal =
+            u_iid + Math.random().toString(36).substring(7) + u_iid;
+          // crypto.randomBytes(64).toString("hex");
+          rFieldVal = crypto.createHash("md5").update(rFieldVal).digest("hex");
+          const token = jwt.sign(
+            {
+              status: "Success",
+              u_id: newUser._id,
+              [u_iid]: rFieldVal,
+            },
+            process.env.TOKEN_SECRET
+          );
+
+          console.log("userRegister   " + token);
+
+          res.cookie("token", token, { httpOnly: true });
+          res.send({ status: "Successful", token: token });
+          await randNumber.updateOne(
+            { u_idHash: u_iid },
+            { jToken: token },
+            { upsert: true },
+            function (err, docs) {
+              if (err) {
+                console.log(err);
+                res.send({ status: "Update Failed" });
+              } else {
+                console.log("Original Doc : ", docs);
+                // return
+              }
+            }
+          );
+        } else {
+          return res.send({ status: "Wrong email or password!" });
+        }
+      });
     }
   })
   
-  await newUser.save(async function (err) {
-    if (!err) {
-      var u_iid = "";
-      u_iid += newUser._id.toString();
-      u_iid = crypto.createHash("md5").update(u_iid).digest("hex");
-      var rFieldVal = u_iid + Math.random().toString(36).substring(7) + u_iid;
-      // crypto.randomBytes(64).toString("hex");
-      rFieldVal = crypto.createHash("md5").update(rFieldVal).digest("hex");
-      const token = jwt.sign(
-        {
-          status: "Success",
-          u_id: newUser._id,
-          [u_iid]: rFieldVal,
-        },
-        process.env.TOKEN_SECRET
-      );
-
-      console.log("userRegister   " + token);
-
-      res.cookie("token", token, { httpOnly: true });
-      res.send({ status: "Successful", token: token });
-      await randNumber.updateOne(
-        { u_idHash: u_iid },
-        { jToken: token },
-        { upsert: true },
-        function (err, docs) {
-          if (err) {
-            console.log(err);
-            res.send({ status: "Update Failed" });
-          } else {
-            console.log("Original Doc : ", docs);
-            // return
-          }
-        }
-      );
-    } else {
-      return res.send({ status: "Wrong email or password!" });
-    }
-  });
+  
 
   
   
